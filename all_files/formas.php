fd7de27c58176b83309b8eac2b4956d67b242679
SvystunDmytro <sdimiks1@gmail.com>
2017-12-04 17:08:37 +0200

    AAAAA

--- all_files/formas.php
+++ all_files/formas.php
@@ -25,31 +25,63 @@
 //        echo " ";
 //    }
 //}
-
-
-function qw($q1)
+function validationForm($data)
 {
-            if (!empty ($_POST ['name']) && empty($_POST['me'])) {
-            }
-            elseif (!empty($_POST['name']) || empty($_POST['me']) == false) {
-                echo "Подписка не оформлена, проверьте введенные данные и попробуйте еще раз";
-            }
-}
+    $error = [];
 
-function qwe($q2)
-{
+    if (empty($data['email'])) {
+        $error['email'] = 'Заполните поле Email';
+    }
+
+    if (empty($data['text'])) {
+        $error['text'] = 'Заполните поле TEXT';
+    }
+
     if (!empty($_FILES ['file']['size'] <= 10485760)) {
-        move_uploaded_file($_FILES['file']['tmp_name'], 'files/' . $_FILES['file']['name']);
+        move_uploaded_file($_FILES['file']['tmp_name'], 'upload/' . $_FILES['file']['name']);
+    } else {
+        $error['file'] = 'Размер файла более 10 Мб';
     }
-    elseif (!empty($_FILES ['file']['size'] > 10485760)) {
-        echo "Подписка не оформлена, размер файла более 10 Мб";
+
+    //Если ошибка то возвращаем массив и выходим из функции через return
+    if ($error) {
+        return [
+            'success' => false,
+            'error' => $error
+        ];
     }
+
+    return [
+        'success' => true,
+        'message' => 'Вы успешно подписались на нас. Спасибо'
+    ];
 }
-function myPOS($po1){
-    if (qw($q1) && qwe($q2) === true ){
-        echo "Вы успешно подписались на нас. Спасибо";
-    }
-}
+//
+//
+//
+//function qw($q1)
+//{
+//            if (!empty ($_POST ['name']) && empty($_POST['me'])) {
+//            }
+//            elseif (!empty($_POST['name']) || empty($_POST['me']) == false) {
+//                echo "Подписка не оформлена, проверьте введенные данные и попробуйте еще раз";
+//            }
+//}
+//
+//function qwe($q2)
+//{
+//    if (!empty($_FILES ['file']['size'] <= 10485760)) {
+//        move_uploaded_file($_FILES['file']['tmp_name'], 'files/' . $_FILES['file']['name']);
+//    }
+//    elseif (!empty($_FILES ['file']['size'] > 10485760)) {
+//        echo "Подписка не оформлена, размер файла более 10 Мб";
+//    }
+//}
+//function myPOS($po1){
+//    if (qw($q1) && qwe($q2) === true ){
+//        echo "Вы успешно подписались на нас. Спасибо";
+//    }
+//}
 ?>
 
 <!DOCTYPE HTML>
@@ -79,13 +111,12 @@ error_reporting(-1); - Зачем он его использует ? -->
             <h2>Форма обратной связи</h2>
             <p> Оставьте свой еmail </p>
             <p>
-                <input required type="email" name="name" placeholder="Ваш еmail" autocomplete="on" class="content"
+                <input  type="email" name="email" placeholder="Ваш еmail" autocomplete="on" class="content" required
                        style="width: 50%; text-align: center; border: 1px solid black; padding: 5px; margin-bottom: 5px;">
             </p>
             <p>Введите свой текст</p>
             <p>
-                <textarea required name="me" placeholder="Ваше сообение" class="content" cols="2"
-                          style="width: 50%; text-align: center; border: 1px solid black; padding: 5px; margin-bottom: 5px;"> </textarea>
+                <textarea name="text" maxlength="200" placeholder="Ваше сообение" class="content" required  style="width: 50%; text-align: center; border: 1px solid black; padding: 5px; margin-bottom: 5px;"> </textarea>
             </p>
             <p>
                 <input required type="file" name="file">
@@ -95,25 +126,22 @@ error_reporting(-1); - Зачем он его использует ? -->
                 <button type="submit" name="send" value="text" class="content" style="text-align: center;"> Подписатся
                 </button>
             </p>
-            <p> <?php echo myPOS($po1);?>
+            <p> <?php
+                $formData = $_POST;
+                $response = validationForm($formData);
+                if ($response['success']) {
+                    echo $response['message'];
+                } else {
+                foreach ($response['error'] as $value) {?>
+                     <p style="color:red"><?=$value?><p>
+                <?}
+                echo '<b style="color:red">Подписка не оформлена.';
+                }?>
             </p>
         </form>
     </div>
 </div>
 
-<?php
-//
-//if (!empty($_FILES ['file']['size'] <= 20000)) {
-//    //var_dump($_FILES);die;
-//    move_uploaded_file($_FILES['file']['tmp_name'], 'files/' . $_FILES['file']['name']);
-//} else {
-//    echo " Загружаемый файл слишком большого размера! ";
-//}
-//echo "<pre>";
-////var_dump($_FILES);
-//echo "</pre>";
-
-?>
 
 </body>
 </html>
